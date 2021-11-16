8957f01b4d31c8b7902b583afdf74758b442452f
Rektedekte <olivervosswinkler@gmail.com>
2021-11-16 09:52:36 +0100

    Moved fonts to seperate file

---
+++ fonts.py
@@ -0,0 +1,9 @@
+"""
+This file acts as a container for all the fonts used in the game.
+It can be viewed like an oop container class, just without the class.
+"""
+
+import pygame
+
+# This is the main font to be used in ui elements
+main = pygame.font.Font(None, 50)
